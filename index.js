870ceae80746da4c1a2d5a423a146801f5978f69
davidh-gif <davidh@ltslogistics.com>
2026-05-27 11:17:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
  
@@ -6,8 +5,8 @@ app.use(express.json())
  
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, anthropic-dangerous-direct-browser-access')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Headers', '*')
   res.header('Access-Control-Max-Age', '86400')
   if (req.method === 'OPTIONS') {
     return res.status(200).end()
